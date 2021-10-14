a2b7ec12d0faec20ed7962f5e996d501252ef865
Okhotnik <vadim2000ohota@gmail.com>
2021-10-14 14:39:56 +0300

    Update IntegrationTest.java

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -7,24 +7,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class IntegrationTest {
     Check check = new Check();
     Count count = new Count();
+    String text;
 
     @Test
     void integrationTest1() {
-        String text = "Maven Good Jobs";
+        text = "Maven Good Jobs";
         assertEquals(true, check.determine(text));
         assertEquals("Counting vowels and consonants:\n" + "Vowels: 5 {1=a, 2=e, 3=o, 4=o, 5=o}\n" + "Consonants: 8 {1=M, 2=v, 3=n, 4=G, 5=d, 6=J, 7=b, 8=s}", count.identify(text));
     }
 
     @Test
     void integrationTest2() {
-        String text = "audi";
+        text = "audi";
         assertEquals(true, check.determine(text));
         assertEquals("Counting vowels and consonants:\n" + "Vowels: 3 {1=a, 2=u, 3=i}\n" + "Consonants: 1 {1=d}", count.identify(text));
     }
 
     @Test
     void integrationTest3() {
-        String text = "Java";
+        text = "Java";
         assertEquals(true, check.determine(text));
         assertEquals("Counting vowels and consonants:\n" + "Vowels: 2 {1=a, 2=a}\n" + "Consonants: 2 {1=J, 2=v}", count.identify(text));
     }
